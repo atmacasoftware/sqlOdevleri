2330cb732c210a2f105dffbcd14154f465153538
Ahmet Atmaca <atmacasoftware52@gmail.com>
2024-03-26 10:38:25 +0300

    ödev10 yapıldı.

---
+++ odev10.sql
@@ -0,0 +1,5 @@
+SELECT city, country FROM city LEFT JOIN country ON city.country_id = country.country_id;
+
+SELECT payment_id, first_name, last_name FROM payment RIGHT JOIN customer ON payment.customer_id = customer.customer_id;
+
+SELECT rental_id, first_name, last_name FROM rental FULL JOIN customer ON customer.customer_id = rental.customer_id;

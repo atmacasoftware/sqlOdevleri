0b768b6bd463c3a29a57d8b8fd8f5f94c195a3a1
Ahmet Atmaca <atmacasoftware52@gmail.com>
2024-03-26 11:06:08 +0300

    odev11 düzenlendi

--- odev11.sql
+++ odev11.sql
@@ -3,3 +3,15 @@
 (SELECT first_name FROM actor) INTERSECT (SELECT first_name FROM customer);
 
 (SELECT first_name FROM actor) EXCEPT (SELECT first_name FROM customer);
+
+(SELECT first_name FROM customer)
+UNION ALL
+(SELECT first_name FROM actor);
+
+SELECT first_name FROM customer
+INTERSECT ALL
+SELECT first_name FROM actor
+
+SELECT first_name FROM customer
+EXCEPT ALL
+SELECT first_name FROM actor
